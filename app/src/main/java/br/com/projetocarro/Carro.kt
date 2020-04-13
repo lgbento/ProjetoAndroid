a2455c1d95d62cde19f29a10a416b03cc533bcbc
Wallisson Marinho <wallissonmarinho@hotmail.com>
2020-04-13 12:50:30 -0300

    imagens adicionado

--- app/src/main/java/br/com/projetocarro/Carro.kt
+++ app/src/main/java/br/com/projetocarro/Carro.kt
@@ -1,4 +1,4 @@
 package br.com.projetocarro
 
-class Carro(var marca:String = "", var modelo:String = "", var preco:String = "", var descricao:String = "") {
+class Carro(var marca:String = "", var modelo:String = "", var preco:String = "", var descricao:String = "", var idResourceImage: Int = 0) {
 }
--- app/src/main/java/br/com/projetocarro/CarroViewHolder.kt
+++ app/src/main/java/br/com/projetocarro/CarroViewHolder.kt
@@ -1,7 +1,9 @@
 package br.com.projetocarro
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CarroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -12,10 +14,12 @@ class CarroViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var textModelo = itemView.findViewById<TextView>(R.id.layout_viewHolder_carro_modelo)
         var textDescricao = itemView.findViewById<TextView>(R.id.layout_viewHolder_carro_descricao)
         var textPreco = itemView.findViewById<TextView>(R.id.layout_viewHolder_carro_preco)
+        var imageView = itemView.findViewById<ImageView>(R.id.layout_viewHolder_carro_image)
 
         textMarca.text = carro.marca
         textModelo.text = carro.modelo
         textDescricao.text = carro.descricao
         textPreco.text = carro.preco
+        imageView.setImageDrawable(ContextCompat.getDrawable(imageView.context, carro.idResourceImage))
     }
 }
